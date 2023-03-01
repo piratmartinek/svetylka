31fe7ffefb73fa26794b8e61f754cd14f2c35bc1
piratmartinek <121960967+piratmartinek@users.noreply.github.com>
2023-03-01 20:09:48 +0100

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,10 +1,24 @@
 input.onButtonPressed(Button.A, function () {
     basic.showString(" znáš hru shadow tactic blades of the shogun?")
-    basic.showString("já jí znám je dobrá")
-    basic.showString("je tam Hayato")
-    basic.showIcon(IconNames.Happy)
+    basic.pause(2000)
+    if (input.buttonIsPressed(Button.A)) {
+        strip.showColor(neopixel.colors(NeoPixelColors.Red))
+        basic.pause(2000)
+        basic.showString("já jí znám je dobrá")
+        basic.showString("je tam Hayato")
+        basic.showIcon(IconNames.Happy)
+    } else if (input.buttonIsPressed(Button.B)) {
+        strip.showColor(neopixel.colors(NeoPixelColors.Violet))
+        basic.showString("fakt?")
+        basic.pause(2000)
+        basic.showString("i ji neznáš?")
+    } else {
+    	
+    }
 })
 input.onButtonPressed(Button.AB, function () {
+    strip.showColor(neopixel.colors(NeoPixelColors.Green))
+    basic.pause(2000)
     basic.showString("jsi chytrý?")
     basic.showString("tak si můžeš zahrát creeks")
     basic.pause(2000)
@@ -27,6 +41,8 @@ input.onButtonPressed(Button.AB, function () {
     basic.showIcon(IconNames.Happy)
 })
 input.onButtonPressed(Button.B, function () {
+    strip.showColor(neopixel.colors(NeoPixelColors.Blue))
+    basic.pause(2000)
     basic.showString("hra ano se odehrává v budoucnosti a ano 1800 v minulosti")
     basic.pause(2000)
     basic.showLeds(`
@@ -47,6 +63,8 @@ input.onButtonPressed(Button.B, function () {
     basic.pause(2000)
 })
 input.onGesture(Gesture.Shake, function () {
+    strip.showColor(neopixel.colors(NeoPixelColors.Purple))
+    basic.pause(2000)
     basic.showString("wow")
     basic.showIcon(IconNames.Surprised)
     basic.pause(2000)
@@ -54,37 +72,51 @@ input.onGesture(Gesture.Shake, function () {
     basic.pause(2000)
     basic.showIcon(IconNames.Happy)
 })
-basic.showString("ahoj!")
-basic.pause(2000)
-basic.showIcon(IconNames.Heart)
-basic.pause(2000)
-basic.showString("kingdom come je dobrá hra")
-basic.pause(2000)
-basic.showLeds(`
-    . . # . .
-    . . # . .
-    . . # . .
-    . # # # .
-    . . # . .
-    `)
-basic.pause(2000)
-basic.showLeds(`
-    . # # # .
-    . # # # .
-    . # # # .
-    . # # # .
-    . . # . .
-    `)
-basic.pause(2000)
-basic.showLeds(`
-    . . . . .
-    . . # . .
-    . # # # .
-    . . # . .
-    . # . # .
-    `)
-basic.pause(2000)
-basic.showString("haha")
-basic.pause(2000)
-basic.showString("pojd si hrát")
-basic.showIcon(IconNames.Happy)
+input.onGesture(Gesture.TiltRight, function () {
+    strip.showColor(neopixel.colors(NeoPixelColors.Yellow))
+    basic.pause(2000)
+    basic.showString("ahoj!")
+    basic.pause(2000)
+    basic.showIcon(IconNames.Heart)
+    basic.pause(2000)
+    basic.showString("kingdom come je dobrá hra")
+    basic.pause(2000)
+    basic.showLeds(`
+        . . # . .
+        . . # . .
+        . . # . .
+        . # # # .
+        . . # . .
+        `)
+    basic.pause(2000)
+    basic.showLeds(`
+        . # # # .
+        . # # # .
+        . # # # .
+        . # # # .
+        . . # . .
+        `)
+    basic.pause(2000)
+    basic.showLeds(`
+        . . . . .
+        . . # . .
+        . # # # .
+        . . # . .
+        . # . # .
+        `)
+    basic.pause(2000)
+    basic.showString("haha")
+    basic.pause(2000)
+    basic.showString("pojd si hrát")
+    basic.showIcon(IconNames.Happy)
+})
+let strip: neopixel.Strip = null
+strip = neopixel.create(DigitalPin.P0, 24, NeoPixelMode.RGB)
+strip.setBrightness(100)
+strip.showRainbow(1, 360)
+for (let index = 0; index < 10; index++) {
+    strip.show()
+    basic.pause(200)
+    strip.rotate(1)
+}
+strip.clear()
